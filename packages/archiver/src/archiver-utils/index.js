aa757774098824b55fb1d8bc20f5684483f214f0
Roman <dev@rman.dev>
2026-04-14 21:59:27 +0100

    lazystream

--- packages/archiver/src/archiver-utils/index.js
+++ packages/archiver/src/archiver-utils/index.js
@@ -6,8 +6,7 @@
  * https://github.com/archiverjs/archiver-utils/blob/master/LICENSE
  */
 var fs = require('graceful-fs');
-var path = require('path');
-var lazystream = require('lazystream');
+var lazystream = require('../lazystream/index.js');
 var normalizePath = require('normalize-path');
 var defaults = require('lodash.defaults');
 
@@ -15,30 +14,6 @@ var Stream = require('stream').Stream;
 var PassThrough = require('node:stream').PassThrough;
 
 var utils = module.exports = {};
-
-utils.collectStream = function(source, callback) {
-  var collection = [];
-  var size = 0;
-
-  source.on('error', callback);
-
-  source.on('data', function(chunk) {
-    collection.push(chunk);
-    size += chunk.length;
-  });
-
-  source.on('end', function() {
-    var buf = new Buffer(size);
-    var offset = 0;
-
-    collection.forEach(function(data) {
-      data.copy(buf, offset);
-      offset += data.length;
-    });
-
-    callback(null, buf);
-  });
-};
 
 utils.dateify = function(dateish) {
   dateish = dateish || new Date();
@@ -93,56 +68,4 @@ utils.sanitizePath = function(filepath) {
 
 utils.trailingSlashIt = function(str) {
   return str.slice(-1) !== '/' ? str + '/' : str;
-};
-
-utils.unixifyPath = function(filepath) {
-  return normalizePath(filepath, false).replace(/^\w+:/, '');
-};
-
-utils.walkdir = function(dirpath, base, callback) {
-  var results = [];
-
-  if (typeof base === 'function') {
-    callback = base;
-    base = dirpath;
-  }
-
-  fs.readdir(dirpath, function(err, list) {
-    var i = 0;
-    var file;
-    var filepath;
-
-    if (err) {
-      return callback(err);
-    }
-
-    (function next() {
-      file = list[i++];
-
-      if (!file) {
-        return callback(null, results);
-      }
-
-      filepath = path.join(dirpath, file);
-
-      fs.stat(filepath, function(err, stats) {
-        results.push({
-          path: filepath,
-          relative: path.relative(base, filepath).replace(/\\/g, '/'),
-          stats: stats
-        });
-
-        if (stats && stats.isDirectory()) {
-          utils.walkdir(filepath, base, function(err, res) {
-            res.forEach(function(dirEntry) {
-              results.push(dirEntry);
-            });
-            next();
-          });
-        } else {
-          next();
-        }
-      });
-    })();
-  });
 };
