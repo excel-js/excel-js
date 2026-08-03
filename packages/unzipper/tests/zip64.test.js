bc56aa86f9e54a7cae2ae42cec624bdd63ca7821
Roman <dev@rman.dev>
2026-08-03 18:48:56 +0100

    Update zip64.test.js

--- packages/unzipper/tests/zip64.test.js
+++ packages/unzipper/tests/zip64.test.js
@@ -1,57 +1,53 @@
 import assert from "node:assert";
 import fs from "node:fs";
-import { desribe, test } from "node:test";
+import { test } from "node:test";
 
 import { Parse } from "@excel.js/unzipper";
 
 const UNCOMPRESSED_SIZE = 5368709120;
 const ZIP64_SIZE = 36;
 
-desribe("Correct uncompressed size for zip64", async (t) => {
+test("Correct uncompressed size for zip64 - in unzipper.parse", async () => {
   const archive = "./tests/fixtures/big.zip";
 
-  await test("in unzipper.parse", async () => {
-    await new Promise((resolve, reject) => {
-      fs.createReadStream(archive)
-        .pipe(Parse())
-        .on("entry", (entry) => {
-          try {
-            assert.strictEqual(
-              entry.vars.uncompressedSize,
-              UNCOMPRESSED_SIZE,
-              "Parse: File header",
-            );
-            resolve();
-          } catch (err) {
-            reject(err);
-          }
-        })
-        .on("error", reject);
-    });
+  await new Promise((resolve, reject) => {
+    fs.createReadStream(archive)
+      .pipe(Parse())
+      .on("entry", (entry) => {
+        try {
+          assert.strictEqual(
+            entry.vars.uncompressedSize,
+            UNCOMPRESSED_SIZE,
+            "Parse: File header",
+          );
+          resolve();
+        } catch (err) {
+          reject(err);
+        }
+      })
+      .on("error", reject);
   });
 });
 
-test("Parse files from regular zip64 format correctly", async (t) => {
+test("Parse files from regular zip64 format correctly - in unzipper.parse", async () => {
   const archive = "./tests/fixtures/zip64.zip";
 
-  await test("in unzipper.parse", async () => {
-    await new Promise((resolve, reject) => {
-      fs.createReadStream(archive)
-        .pipe(Parse())
-        .on("entry", (entry) => {
-          try {
-            assert.strictEqual(
-              entry.vars.uncompressedSize,
-              ZIP64_SIZE,
-              "Parse: File header",
-            );
-          } catch (err) {
-            reject(err);
-          }
-        })
-        .on("close", resolve)
-        .on("finish", resolve)
-        .on("error", reject);
-    });
+  await new Promise((resolve, reject) => {
+    fs.createReadStream(archive)
+      .pipe(Parse())
+      .on("entry", (entry) => {
+        try {
+          assert.strictEqual(
+            entry.vars.uncompressedSize,
+            ZIP64_SIZE,
+            "Parse: File header",
+          );
+        } catch (err) {
+          reject(err);
+        }
+      })
+      .on("close", resolve)
+      .on("finish", resolve)
+      .on("error", reject);
   });
 });
